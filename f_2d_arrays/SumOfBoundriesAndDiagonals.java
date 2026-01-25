a462d20bc98598c48644da35a1f917672192d81d
amankumar89 <amankumaroo784@gmail.com>
2026-01-25 20:13:00 +0530

    2D array code: SpiralPrint, SumOfBoundriesAndDiagonals, and WavePrint, implementing functionalities for printing elements in a spiral order, calculating the sum of boundaries and diagonals, and printing elements in a wave pattern, respectively.

---
+++ f_2d_arrays/SumOfBoundriesAndDiagonals.java
@@ -0,0 +1,46 @@
+
+public class SumOfBoundriesAndDiagonals {
+
+	public static void totalSum(int[][] arr) {
+		if(arr.length == 0) {
+			System.out.println(0);
+			return;
+		}
+		
+		int diagonalSum1 = 0;
+		int i = 0;
+		int j = 0;
+		while(i < arr.length && j < arr[0].length) {
+			diagonalSum1 += arr[i][j];
+			i++;
+			j++;
+		}
+		
+		int diagonalSum2 = 0;
+		i = 0;
+		j = arr[0].length-1;
+		while(i < arr.length && j >= 0) {
+			if(i != j) {
+				diagonalSum2 += arr[i][j];
+			}
+			i++;
+			j--;
+		}
+		
+		int boundrySum = 0;
+		
+		for (int k = 1; k < arr.length-1; k++) {
+			// top
+			boundrySum += arr[0][k];
+			// right
+			boundrySum += arr[k][arr.length-1];
+			// bottom
+			boundrySum += arr[arr.length-1][k];
+			// left
+			boundrySum += arr[k][0];
+		}
+		
+		System.out.println(diagonalSum1 + diagonalSum2 + boundrySum);
+	}
+
+}
---
+++ f_2d_arrays/WavePrint.java
@@ -0,0 +1,21 @@
+
+public class WavePrint {
+
+	public static void wavePrint(int arr[][]){
+        if(arr.length == 0){
+            return;
+        }
+        for (int i = 0; i < arr[0].length; i++) {
+			if(i % 2 == 0) {
+				for (int j = 0; j < arr.length; j++) {
+					System.out.print(arr[j][i] +" ");
+				}
+			}else {
+				for (int j = arr.length-1; j >= 0; j--) {
+					System.out.print(arr[j][i]+" ");
+				}
+			}
+		}
+	}
+
+}
